9241ac274bb9911bd31ec8fa958db3e765b3b1ac
Poh Kah Kong <kahkong87mail@gmail.com>
2016-09-17 00:38:39 +0800

    Update Buddy

--- app/helpers/buddy_helper.rb
+++ app/helpers/buddy_helper.rb
@@ -6,11 +6,9 @@ module BuddyHelper
     execution_status = (execution_status == 'INPROGRESS' ? 'SUCCESSFUL' : execution_status)
     execution_branch_name = json[:execution][:branch][:name]
     invoker_name = json[:invoker][:name]
-    execution_to_revision_message = json[:execution][:to_revision][:message]
+    execution_to_revision_message = json[:execution][:to_revision][:message].strip
     execution_to_revision_url = json[:execution][:to_revision][:url]
     
-    message =  "[#{project_name}] #{invoker_name} push in '#{execution_to_revision_message}' commit to #{execution_branch_name} branch and the build is #{execution_status}.\n\n"
-    message += "Commit url: #{execution_to_revision_url}\n\n"
-    message += "Build url: #{execution_url}"
+    message = "[#{project_name}] #{invoker_name} push in '#{execution_to_revision_message}' commit to #{execution_branch_name} branch and the build is #{execution_status}.\n\n"
   end
 end
