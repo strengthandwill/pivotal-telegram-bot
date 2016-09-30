645e79886909b4453ff72c8c6ee05c01867496d3
Poh Kah Kong <kahkong87mail@gmail.com>
2016-09-30 11:37:25 +0800

    Update ActivityHelper

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -8,34 +8,37 @@ module ActivityHelper
     story_type        = primary_resource[:story_type]
     story_name        = primary_resource[:name]
     url               = primary_resource[:url]
-    who_did_it        = "[#{project_name}] #{username} #{highlight}"
+    next_state        = next_state(highlight)
+    who_did_it        = "[#{project_name}]<#{next_state}> #{username} #{highlight}"
     
     case json[:kind]
       when 'story_create_activity'
         "#{who_did_it} new #{story_type} \"#{story_name}\".\n#{url}"
       when 'story_update_activity', 'story_delete_activity'
-        return if highlight == 'estimated' || 
-                  highlight == 'finished'  || 
-                  highlight == 'delivered' || 
-                  highlight == 'delivered'
-        if highlight == "edited"
-          # description = json[:changes][0][:new_values][:description]
-          # "#{who_did_it} description to \"#{description}\" of #{story_type} \"#{story_name}\".\n#{url}" unless description.blank?          
-        else
-          "#{who_did_it} #{story_type} \"#{story_name}\".\n#{url}"
-        end
-      when 'comment_create_activity' || 'comment_update_activity'
-        text = json[:changes][0][:new_values][:text]
-        "#{who_did_it} \"#{text}\" to #{story_type} \"#{story_name}\".\n#{url}" unless text.blank?
-      when 'task_update_activity', 'story_delete_activity'
-        s = json[:message].index('"')
-        task_name = json[:message][s..-1]
-        "#{who_did_it} #{task_name} of #{story_type} \"#{story_name}\".\n#{url}"        
-      when 'epic_create_activity'
-        "#{who_did_it} new #{primary_resource[:kind]} \"#{story_name}\".\n#{url}"
+        return if highlight == 'estimated' || highlight == "edited"
+        "#{who_did_it}#{next_state} #{story_type} \"#{story_name}\", #{next_state}\n#{url}"
       else
         logger.info("App -- Undefined kind : #{json}")
         return
+    end
+  end
+  
+  def next_state(highlight)
+    case highlight
+      when "planned"
+        "Ready for DEV"      
+      when "started"
+        "WIP"      
+      when "finished"
+        "Ready for TESTING"
+      when "delivered"
+        "Ready for ACCEPTANCE"
+      when "accepted"
+        "DONE"
+      when "rejected"
+        "Restarted for DEV"
+      else
+        ""
     end
   end
 end
